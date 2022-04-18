920fcecb39693ef51a58839def2105c8b2f25b13
rudghska <rudghska@gmail.com>
2022-04-19 02:19:58 +0900

    대문자로 통일

--- 자바스크립트 알고리즘/section1/대문자로통일.js
+++ 자바스크립트 알고리즘/section1/대문자로통일.js
@@ -1,5 +1,22 @@
 function mySolution(str) {
   return str.toUpperCase();
 }
+function solution(str) {
+  let answer = '';
+
+  for (const c of str) {
+    let codeNumber = c.charCodeAt();
+
+    if (c === c.toUpperCase()) {
+      answer += c;
+    } else {
+      answer += String.fromCharCode(codeNumber - 32);
+    }
+    String.fromCodePoint;
+  }
+
+  return answer;
+}
 
 console.log(mySolution('ItisTimeToStudy'));
+console.log(solution('ItisTimeToStudy'));
