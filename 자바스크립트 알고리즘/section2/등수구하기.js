4624e04be696b01843238d4f42c0e82c68b6705f
rudghska <rudghska@gmail.com>
2022-05-03 04:29:54 +0900

    등수구하기

--- 자바스크립트 알고리즘/section2/등수구하기.js
+++ 자바스크립트 알고리즘/section2/등수구하기.js
@@ -34,5 +34,20 @@ function mySolution2(arr) {
   return answer;
 }
 
+function solution(arr) {
+  const answer = Array.from({ length: arr.length }, () => 1);
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        answer[i]++;
+      }
+    }
+  }
+
+  return answer;
+}
+
 mySolution1([87, 89, 92, 100, 100, 100, 76]);
 mySolution2([87, 89, 92, 100, 100, 100, 76]);
+solution([87, 89, 92, 100, 100, 100, 76]);
