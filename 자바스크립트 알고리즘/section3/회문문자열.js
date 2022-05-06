d41f5a89af9e7d8341fb9004d987ecb4dd2fcfbf
rudghska <rudghska@gmail.com>
2022-05-07 07:16:01 +0900

    회문 문자열

---
+++ 자바스크립트 알고리즘/section3/회문문자열.js
@@ -0,0 +1,18 @@
+function mySolution1(str) {
+  const result = str.split('').reverse().join('');
+  const answer = result.toLowerCase() === str.toLowerCase() ? 'YES' : 'NO';
+  return answer;
+}
+
+function mySolution2(str) {
+  const length = str.length;
+  let result = [];
+  for (let i = 0; i < length; i++) {
+    result[i] = str[length - i - 1];
+  }
+
+  const answer = result.join('').toLowerCase() === str.toLowerCase() ? 'YES' : 'NO';
+}
+
+mySolution1('gooG');
+mySolution2('gooG');
