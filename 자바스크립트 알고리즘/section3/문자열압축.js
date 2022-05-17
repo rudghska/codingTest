ebb0b50fd51d50f73656838fcb17ed3a73526514
rudghska <rudghska@gmail.com>
2022-05-18 02:16:16 +0900

    문자열 압축

--- 자바스크립트 알고리즘/section3/문자열압축.js
+++ 자바스크립트 알고리즘/section3/문자열압축.js
@@ -22,4 +22,25 @@ function mySolution(str) {
   return answer;
 }
 
+function solution(str) {
+  let count = 1;
+  let answer = '';
+
+  str = str + ' ';
+
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i] == str[i + 1]) {
+      count++;
+    } else {
+      answer += str[i];
+      if (count > 1) answer += count;
+      count = 1;
+    }
+  }
+
+  console.log(answer);
+  return answer;
+}
+
 mySolution('KKHSSSSSSSE');
+solution('KKHSSSSSSSE');
