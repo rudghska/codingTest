3d1ba4703e5d4514410b5ae4e96c3ef7e94c53da
rudghska <rudghska@gmail.com>
2022-06-15 00:36:14 +0900

    졸업선물

---
+++ 자바스크립트 알고리즘/section4/졸업선물.js
@@ -0,0 +1,33 @@
+function mySolution(m, arr) {
+  let n = 1;
+  let sum = 0;
+  let max = [];
+  let answer;
+  arr.sort((a, b) => a[0] - b[0]);
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i][0] / 2 + arr[i][1];
+    for (let j = 0; j < arr.length; j++) {
+      if (i !== j && sum < m) {
+        sum += arr[j][0] + arr[j][1];
+        n++;
+      }
+    }
+
+    max.push(n);
+    sum = 0;
+    n = 0;
+  }
+
+  max.sort((a, b) => a - b);
+  answer = max[max.length - 1];
+  return answer;
+}
+
+mySolution(28, [
+  [6, 6],
+  [2, 2],
+  [4, 3],
+  [4, 5],
+  [10, 3],
+]);
