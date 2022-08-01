7eee0f628ada3721a3834bff17fcab08b5160da4
rudghska <rudghska@gmail.com>
2022-08-01 23:25:35 +0900

    땅따먹기.js

---
+++ programmers/level2/땅따먹기.js
@@ -0,0 +1,12 @@
+function solution(land) {
+  const len = land.length;
+
+  for (var i = len - 2; i >= 0; i--) {
+    land[i][0] = Math.max(land[i + 1][1], land[i + 1][2], land[i + 1][3]) + land[i][0];
+    land[i][1] = Math.max(land[i + 1][0], land[i + 1][2], land[i + 1][3]) + land[i][1];
+    land[i][2] = Math.max(land[i + 1][0], land[i + 1][1], land[i + 1][3]) + land[i][2];
+    land[i][3] = Math.max(land[i + 1][0], land[i + 1][1], land[i + 1][2]) + land[i][3];
+  }
+
+  return Math.max(...land[0]);
+}
